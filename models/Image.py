482cf97fbff3010635e3f0ada645d77abe74d060
Hugo José Bello <hugojose@Hugos-MacBook-Pro.local>
2020-09-13 12:43:32 +0200

    new model

--- models/Image.py
+++ models/Image.py
@@ -1,10 +1,18 @@
 from models.Config import Config
+import datetime
 
 
 class Image:
-    def __init__(self, config: Config):
+    def __init__(self, config: Config, path):
         self.config = config
+        self.path = path
+        self.device_id = config.device_id
+        self.sensor_id = config.device_id
+        self.type= "image"
+        self.date = datetime.datetime.now()
+        self.filename = "image_" + self.device_id + "_" + self.sensor_id + str(self.date)
+
 
     def upload_image(self, path):
-        image_blob = self.config.bucket.blob(path)
+        image_blob = self.config.bucket.blob(self.filename)
         image_blob.upload_from_filename(path)
