d0586211e45c7103e32cf5cd05116005bafc345f
Hugo José Bello <hugojose@Hugos-MacBook-Pro.local>
2020-09-14 17:26:20 +0200

    new

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from sensors.Camera import Camera
 from models.Config import Config
 from apscheduler.schedulers.background import BackgroundScheduler
-
+import time
 import os
 
 def run_sensors():
@@ -14,10 +14,17 @@ def main():
 
     print("starting process")
     sched = BackgroundScheduler()
-    sched.daemonic = False
     sched.start()
     sched.add_job(run_sensors, 'interval', seconds=10)
     print("eding process")
+
+    try:
+        # This is here to simulate application activity (which keeps the main thread alive).
+        while True:
+            time.sleep(5)
+    except (KeyboardInterrupt, SystemExit):
+        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        sched.shutdown()
 
 if __name__ == '__main__':
 	main()
